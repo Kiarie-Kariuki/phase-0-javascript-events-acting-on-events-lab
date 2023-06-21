a63188f9a3e0ba9ebb0902ec8830ae2c6c0e1038
Collins <collinskiarie13@outlook.com>
2023-06-21 17:03:50 +0300

    Completed task

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  // Add your event listener for the key 'ArrowRight' to make Dodger go right
+
+  function moveDodgerRight() {
+    var dodger = document.getElementById('dodger');
+    var currentPosition = parseInt(dodger.style.left);
+    var newPosition = currentPosition + 1;
+  
+    dodger.style.left = newPosition + 'px';
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  })
+  
